0a9ca338c448883bb6c7bf92eb3364b6133b339a
Andrea <guerreroaanathalia@gmail.com>
2026-02-08 17:15:58 -0500

    chore: setup Tailwind and add basic Navbar

--- src/app/core/layouts/navbar/navbar.component.ts
+++ src/app/core/layouts/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @Component({
   selector: 'app-navbar',
  imports: [MatButtonModule,
-  MatIconModule
+  MatIconModule,
+  MatToolbarModule
  ],
  templateUrl: './navbar.component.html',
 })
